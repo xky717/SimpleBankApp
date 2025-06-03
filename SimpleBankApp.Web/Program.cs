5edf399fb843966cfe1f26b34f5e4e9766238fe6
xky717 <alimu.xiaokeya@tu-dortmund.de>
2025-06-03 22:16:08 +0200

    Implement user registration/login functionality: AccountController, views, and Cookie authentication

--- SimpleBankApp.Web/Program.cs
+++ SimpleBankApp.Web/Program.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;  
+using Microsoft.AspNetCore.Authentication.Cookies;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +13,15 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllersWithViews();
+
+builder.Services
+    .AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/Account/Login";
+        options.LogoutPath = "/Account/Logout";
+        options.ExpireTimeSpan = TimeSpan.FromHours(1);
+    });
 
 var app = builder.Build();
 
@@ -23,8 +34,8 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
-// app.UseAuthentication(); // 如果以后加身份验证
-// app.UseAuthorization();
+ app.UseAuthentication(); // 如果以后加身份验证
+ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
